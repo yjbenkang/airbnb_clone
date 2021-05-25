2b1431cbc23c256d157be42fdbe2d745d0dfda95
YongJinKang <kyongjin1234@gmail.com>
2021-05-25 21:39:18 +0900

    #14.1 Form and CSRF!

--- users/views.py
+++ users/views.py
@@ -5,8 +5,11 @@ from . import forms
 
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "kyongjin1234@naver.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
-        pass
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        return render(request, "users/login.html", {"form": form})
