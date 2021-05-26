65a613a7796b946936d28322e2cb769f0b3a1715
YongJinKang <kyongjin1234@gmail.com>
2021-05-26 19:15:31 +0900

    #16.0~1 Setting up Mailgun and Dotenv

--- config/settings.py
+++ config/settings.py
@@ -156,3 +156,11 @@ MEDIA_ROOT = os.path.join(
 )
 
 MEDIA_URL = "/media/"
+
+
+# Email Configuration
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
--- users/models.py
+++ users/models.py
@@ -32,3 +32,11 @@ class User(AbstractUser):
         choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW
     )
     superhost = models.BooleanField(default=False)
+    email_confirmed = models.BooleanField(default=False)
+    email_secret = models.CharField(
+        max_length=120,
+        default="",
+    )
+
+    def verify_email(self):
+        pass
