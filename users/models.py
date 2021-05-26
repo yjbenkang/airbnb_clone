bd30a810939f6cd99509fd9605d4b8651bee5d4a
YongJinKang <kyongjin1234@gmail.com>
2021-05-26 22:35:31 +0900

    #17.1 Github Login part.2

--- users/models.py
+++ users/models.py
@@ -26,6 +26,16 @@ class User(AbstractUser):
 
     CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGING_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"),
+        (LOGIN_GITHUB, "Github"),
+        (LOGING_KAKAO, "Kakao"),
+    )
+
     avatar = models.ImageField(blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
@@ -41,6 +51,9 @@ class User(AbstractUser):
     email_secret = models.CharField(
         max_length=120,
         default="",
+    )
+    login_method = models.CharField(
+        max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
     )
 
     def verify_email(self):
