12ce687bed3a34a590c0b28437b9ec9fc10b9684
YongJinKang <kyongjin1234@gmail.com>
2021-05-29 19:21:33 +0900

    #20.1~20.5 TailWindCSS Header, Room Card pt.1

--- config/settings.py
+++ config/settings.py
@@ -146,14 +146,11 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 AUTH_USER_MODEL = "users.User"
 
-
-MEDIA_ROOT = os.path.join(
-    BASE_DIR,
-    "uploads",
-)
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
 
