a67e78df60dc9013fb06ee86f92040c86e36b71d
YongJinKang <kyongjin1234@gmail.com>
2021-06-05 17:01:11 +0900

    #24.3 Finishing Calendar

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from django.utils import timezone
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
@@ -125,8 +125,12 @@ class Room(core_models.TimeStampedModel):
         return photos
 
     def get_calendars(self):
-        present_month = datetime.today().month
-        present_year = datetime.today().year
-        this_month = Calendar(int(present_year), int(present_month))
-        next_month = Calendar(int(present_year), int(present_month) + 1)
-        return [this_month, next_month]
+        now = timezone.now()
+        this_year = now.year
+        this_month = now.month
+        next_month = this_month + 1
+        if this_month == 12:
+            next_month = 1
+        this_month_cal = Calendar(this_year, this_month)
+        next_month_cal = Calendar(this_year, next_month)
+        return [this_month_cal, next_month_cal]
