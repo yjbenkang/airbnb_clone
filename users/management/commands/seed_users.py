bfd7cbfbf82ba18a0b69da7a177d5f157a52023a
YongJinKang <kyongjin1234@gmail.com>
2021-06-17 21:25:24 +0900

    !

--- users/management/commands/seed_users.py
+++ users/management/commands/seed_users.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django_seed import Seed
 from users.models import User
-
+from faker import Faker
 
 class Command(BaseCommand):
 
@@ -14,7 +14,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         number = options.get("number")
-        seeder = Seed.seeder()
-        seeder.add_entity(User, number, {"is_staff": False, "is_superuser": False})
+        seeder = Seed.seeder(locale='ko_KR')
+        faker = Faker('ko_KR')
+        
+        seeder.add_entity(User, number, {
+            "first_name": lambda x: faker.name()[0:1],
+            "last_name": lambda x: faker.name()[1:4],
+            "is_staff": False,
+            "is_superuser": False
+        })
         seeder.execute()
         self.stdout.write(self.style.SUCCESS(f"{number} users created!"))
