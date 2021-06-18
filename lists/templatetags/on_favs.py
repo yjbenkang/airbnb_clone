2a4491428d7dbb20fdda3a0913c93c5ce19eab0f
YongJinKang <kyongjin1234@gmail.com>
2021-06-18 15:59:12 +0900

    change templatetags/on_favs.py

--- lists/templatetags/on_favs.py
+++ lists/templatetags/on_favs.py
@@ -7,7 +7,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def on_favs(context, room):
     user = context.request.user
-    the_list = list_models.List.objects.get_or_none(user=user, name="내가 좋아하는 방 목록")
+    the_list = list_models.List.objects.get_or_none(user=user, name=".Favs")
     print(the_list)
     if the_list is not None:
         print(the_list)
